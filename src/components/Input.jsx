14ccafadfd6b469cd8ebeddcea648dfd57b72f89
Liv <w.phrush@gmail.com>
2019-09-05 15:23:36 -0500

    fix file types

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,11 @@
+import React from 'react';
+
 export const Input = ({ content }) => {
+
   return (
     <div className="input-container">
       <label className="input-label" htmlFor="" >{content}</label>
-      <input className="input" type="text" name={content} id={content} />
+      <input className="input" placeholder={content} type="text" name={content} id={content} />
     </div>
   );
 };
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import '../../css/style.css';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+// import { action } from '@storybook/addon-actions';
+// import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
+// import { Button, Welcome } from '@storybook/react/demo';
+import { Input } from '../components/Input';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Components', module)
+  .add('Input', () => <Input content="Option" />);
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+// storiesOf('Button', module)
+//   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+//   .add('with some emoji', () => (
+//     <Button onClick={action('clicked')}>
+//       <span role="img" aria-label="so cool">
+//         😀 😎 👍 💯
+//       </span>
+//     </Button>
+//   ));
