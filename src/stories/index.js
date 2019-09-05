fa0ec5831026ccfcbdf2b82f02bfe9f6528c62e0
Liv <w.phrush@gmail.com>
2019-09-05 15:25:32 -0500

    clean

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,21 +2,8 @@ import React from 'react';
 import '../../css/style.css';
 
 import { storiesOf } from '@storybook/react';
-// import { action } from '@storybook/addon-actions';
-// import { linkTo } from '@storybook/addon-links';
 
-// import { Button, Welcome } from '@storybook/react/demo';
 import { Input } from '../components/Input';
 
 storiesOf('Components', module)
   .add('Input', () => <Input content="Option" />);
-
-// storiesOf('Button', module)
-//   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-//   .add('with some emoji', () => (
-//     <Button onClick={action('clicked')}>
-//       <span role="img" aria-label="so cool">
-//         😀 😎 👍 💯
-//       </span>
-//     </Button>
-//   ));
