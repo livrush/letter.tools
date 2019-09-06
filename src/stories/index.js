93856235ad0277885027e1802da09151a4aa444a
Liv <w.phrush@gmail.com>
2019-09-05 20:44:15 -0500

    add new components to storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,27 +1,41 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import centered from '@storybook/addon-centered/react';
-import { faCheckSquare, faCalculator } from '@fortawesome/free-solid-svg-icons'
+import { faCat } from '@fortawesome/free-solid-svg-icons'
 
 import '../css/style.css';
 
 import {
+  ActionBar,
   Button,
   Input,
   ButtonGroup,
+  InjectionColumn,
 } from '../components';
+
+storiesOf('Components/Button', module)
+  .addDecorator(centered)
+  .add('Action Bar', () => <ActionBar />);
+
+storiesOf('Components/Button', module)
+  .addDecorator(centered)
+  .add('Icon', () => <Button icon={faCat} />)
+  .add('Text', () => <Button text="Click me!" />)
+  .add('Group', () => <ButtonGroup config={[
+    { text: 'Button' },
+    { icon: faCat },
+    {
+      icon: faCat,
+      text: 'Cat',
+    },
+  ]} />);
 
 storiesOf('Components', module)
   .addDecorator(centered)
-  .add('Button/Icon', () => <Button icon={faCheckSquare} />)
-  .add('Button/Text', () => <Button text="Click me!" />)
-  .add('Button/Group', () => <ButtonGroup config={[
-    { text: 'Button' },
-    { icon: faCheckSquare },
-    { icon: faCalculator },
-    {
-      icon: faCalculator,
-      text: 'Calculator',
-    },
+  .add('InjectionColumn', () => <InjectionColumn config={[
+    { content: 'Costa Rican Colon' },
+    { content: 'Generic Chips' },
+    { content: 'Key' },
+    { content: 'Orchestrator' },
   ]} />)
   .add('Input', () => <Input content="Option" />);
