b0b26defd03c3c190c719197b587d562d469e082
Liv <w.phrush@gmail.com>
2019-09-05 20:43:45 -0500

    update what button shows

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-export const Button = ({ icon, text }) => {
+export const Button = ({ icon, text, ...rest }) => {
   const IconComponent = icon ?
     <FontAwesomeIcon className="button-icon" icon={icon} /> :
     null;
+
+  const TextComponent = text ?
+    <span className="button-text">{text}</span> :
+    null;
+
   return (
-  <button className="button">
+  <button className="button" {...rest} >
     { IconComponent }
-    <span className="button-text">
-      {text}
-    </span>
+    { TextComponent }
   </button>
 )};
