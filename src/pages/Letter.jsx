47865ddde009992cc631a7d8e3e39345b3115912
Liv <w.phrush@gmail.com>
2019-09-07 14:38:01 -0500

    add basic component structure for pages

---
+++ src/pages/Letter.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const LetterPage = () => {
+  return (<div>Letter</div>)
+}
---
+++ src/pages/List.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const ListPage = () => {
+  return (<div>List</div>)
+}
