4001d9c2eb85d96b25f6331dab6f928ab95b3cea
Liv <w.phrush@gmail.com>
2019-09-08 12:52:41 -0500

    ijmplement replaceVariables function

--- src/lib/readVariables.js
+++ src/lib/readVariables.js
@@ -1,11 +1,16 @@
-const regexpWithBraces = /{{.*?}}/g;
+// const regexpWithBraces = /{{.*?}}/g;
 const regexpWithoutBraces = /(?<=\{{).*(?=\}})/g;
 
 export const readVariables = (string, regexp = regexpWithoutBraces) => {
   const matches = string.match(regexp);
-  return [...matches];
+  return [...matches.map(m => m.trim())];
 };
 
-export const replaceVariables = (string, variables = {}, regexp = regexpWithBraces) => {
-  
+export const replaceVariables = (string, variables = {}) => {
+  let key, newString = string;
+  for (key in variables) {
+    let regexp = new RegExp(`{{\\s*${key}\\s*}}`, 'g');
+    newString = newString.replace(regexp, variables[key]);
+  }
+  return newString;
 };
