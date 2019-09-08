2a7e3d036e2947302f0893848be2b639981258d9
Liv <w.phrush@gmail.com>
2019-09-07 22:21:21 -0500

    add actions

---
+++ src/redux/actions.js
@@ -0,0 +1,62 @@
+import {
+  UPDATE_VARIABLE,
+  UPDATE_LETTER_TITLE,
+  UPDATE_LETTER_CONTENT,
+  EDIT_LETTER_CONTENT,
+  VIEW_LETTER_CONTENT,
+} from './constants';
+
+// ! ////////////////////////////
+// ! Variables //////////////////
+// ! ////////////////////////////
+
+function updateVariable(updatedVariable) {
+  return {
+    type: UPDATE_VARIABLE,
+    payload: updatedVariable,
+  };
+}
+
+// ! ////////////////////////////
+// ! Letter /////////////////////
+// ! ////////////////////////////
+
+function updateLetterTitle(newTitle) {
+  return {
+    type: UPDATE_LETTER_TITLE,
+    payload: { newTitle },
+  }
+}
+
+// todo: This is used in variables reducer, make sure it works
+function updateLetterContent({ newContent, newVariableNames }) {
+  return {
+    type: UPDATE_LETTER_CONTENT,
+    payload: {
+      newContent,
+      newVariableNames,
+    },
+  }
+}
+
+function editLetterContent() {
+  return {
+    type: EDIT_LETTER_CONTENT,
+    payload: {},
+  }
+}
+
+function viewLetterContent() {
+  return {
+    type: VIEW_LETTER_CONTENT,
+    payload: {},
+  }
+}
+
+export {
+  updateVariable,
+  updateLetterTitle,
+  updateLetterContent,
+  editLetterContent,
+  viewLetterContent,
+};
