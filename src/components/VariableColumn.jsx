2802d57edac0d09f9ed7a7735868c862251770ca
Liv <w.phrush@gmail.com>
2019-09-07 21:53:49 -0500

    update prop types

--- src/components/VariableColumn.jsx
+++ src/components/VariableColumn.jsx
@@ -2,23 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Input } from '.';
 
-const VariableColumn = ({ config }) => (
-  <div className="injections-container">
-    <h2 className="injections-title">Injections</h2>
-    {
-      config.map(({ content }) => (
-        <Input key={content} content={content} />
-      ))
-    }
-  </div>
-)
+const VariableColumn = ({ config }) => {
+  const InputComponents = [];
+  let variableName;
+
+  for (variableName in config) {
+    const variableValue = config[variableName]
+    InputComponents.push(<Input key={variableName} variable={{ name: variableName, value: variableValue }} />);
+  }
+
+  return (
+    <div className="injections-container">
+      <h2 className="injections-title">Injections</h2>
+      { InputComponents }
+    </div>
+  );
+};
 
 VariableColumn.propTypes = {
-  config: PropTypes.arrayOf(
-    PropTypes.shape({
-      content: PropTypes.string,
-    }).isRequired
-  ).isRequired,
+  config: PropTypes.objectOf(PropTypes.string).isRequired,
 };
 
 export { VariableColumn };
