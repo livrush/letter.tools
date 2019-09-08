5afe05b282371f418c19f5eb8f7d49791c4e52fc
Liv <w.phrush@gmail.com>
2019-09-08 13:30:55 -0500

    Updates

---
+++ precache-manifest.36468e47283f0d9a168264d3ca4a4efd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f83667f0fc440c45e616503f9b5bb96d",
+    "url": "/letter.tools/index.html"
+  },
+  {
+    "revision": "a1b6f9fe87eb79bae8f7",
+    "url": "/letter.tools/static/css/main.0d840a3a.chunk.css"
+  },
+  {
+    "revision": "263dc89da55a06f48769",
+    "url": "/letter.tools/static/js/2.0ab9a185.chunk.js"
+  },
+  {
+    "revision": "a1b6f9fe87eb79bae8f7",
+    "url": "/letter.tools/static/js/main.c6f53295.chunk.js"
+  },
+  {
+    "revision": "0bb4e1f2e77fbca4ec73",
+    "url": "/letter.tools/static/js/runtime~main.e1a3611c.js"
+  }
+]);
