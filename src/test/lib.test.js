16d46f910f2725a3e400c935454c5b1e93623b10
Liv <w.phrush@gmail.com>
2019-09-08 13:01:13 -0500

    add faker to tests to randomize results

--- src/test/lib.test.js
+++ src/test/lib.test.js
@@ -8,12 +8,24 @@ import {
 
 describe('readVariables', function () {
   it('should return one value', function () {
-    const variables = readVariables('Hello {{value}}');
-    const result = [ 'value' ];
+    const value = faker.random.word();
+    const variables = readVariables(`Hello {{${value}}}`);
+    const result = [ value ];
     expect(variables).to.deep.equal(result);
   });
 
-  it('should return values from instructions', function () {
+  it('should return one value trimmed', function () {
+    const value = faker.random.word();
+    const variables1 = readVariables(`Hello {{ ${value}}}`);
+    const variables2 = readVariables(`Hello {{${value} }}`);
+    const variables3 = readVariables(`Hello {{ ${value} }}`);
+    const result = [ value ];
+    expect(variables1).to.deep.equal(result);
+    expect(variables2).to.deep.equal(result);
+    expect(variables3).to.deep.equal(result);
+  });
+
+  it('should return all values from instructions', function () {
     const variables = readVariables(instructions);
     const result = [ 'salutation', 'variable', 'regards' ];
     expect(variables).to.deep.equal(result);
@@ -22,38 +34,42 @@ describe('readVariables', function () {
 
 describe('replaceVariables', function () {
   it('should replace one value', function () {
-    const string = 'We don\'t talk {{place}}'
-    const variables = { place: 'together' };
+    const string = 'We don\'t {{verb}} together'
+    const verb = faker.hacker.verb();
+    const variables = { verb };
     const result = replaceVariables(string, variables);
-    expect(result).to.equal('we don\'t talk together');
+    expect(result).to.equal(`We don\'t ${verb} together`);
   });
 
   it('should replace one value regardless of whitespace', function () {
-    const string = 'We don\'t talk {{ place }}'
-    const variables = { place: 'together' };
+    const string = 'We don\'t {{ verb }} together'
+    const verb = faker.hacker.verb();
+    const variables = { verb };
     const result = replaceVariables(string, variables);
-    expect(result).to.equal('we don\'t talk together');
+    expect(result).to.equal(`We don\'t ${verb} together`);
   });
 
   it('should replace multiple values', function () {
-    const name = faker.name.findName();
     const string = '{{salutation}} How are you doing {{name}}?'
+    const name = faker.name.findName();
     const variables = { salutation: 'Greetings!', name };
     const result = replaceVariables(string, variables);
     expect(result).to.equal(`Greetings! How are you doing ${name}?`);
   });
 
   it('should replace values multiple times', function () {
-    const string = 'I love {{person}} and {{person}} love me'
-    const variables = { person: 'you' };
+    const string = 'I {{verb}} you and you {{verb}} me';
+    const verb = faker.hacker.verb();
+    const variables = { verb };
     const result = replaceVariables(string, variables);
-    expect(result).to.equal('I love you and you love me');
+    expect(result).to.equal(`I ${verb} you and you ${verb} me`);
   });
 
   it('should replace values multiple times regardless of whitespace', function () {
-    const string = 'I love {{ person}} and {{person }} love me'
-    const variables = { person: 'you' };
+    const string = 'I {{  verb }} you and you {{verb }} me';
+    const verb = faker.hacker.verb();
+    const variables = { verb };
     const result = replaceVariables(string, variables);
-    expect(result).to.equal('I love you and you love me');
+    expect(result).to.equal(`I ${verb} you and you ${verb} me`);
   });
 });
