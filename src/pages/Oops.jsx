1e86a293a14328a69a515bbd69e5bacc9329d9d3
Liv <w.phrush@gmail.com>
2019-09-10 00:13:06 -0500

    update copy

--- src/pages/Oops.jsx
+++ src/pages/Oops.jsx
@@ -13,7 +13,7 @@ class OopsPage extends React.Component {
           <img className="envelope-img" src="/img/envelope.svg" alt="envelope" />
           <img className="blink envelope-shock-lines" src="/img/envelope--blink.svg" alt="shock lines" />
         </div>
-        <h1>404 Page Not Found</h1>
+        <h1>404 Not Found</h1>
         <h2>The page you're requesting could not be found.</h2>
       </div>
     );
