15d6b62fcaace909263d1f08f84df9fb93213d8a
Liv <w.phrush@gmail.com>
2019-09-09 20:39:05 -0500

    add stand alone textarea component

---
+++ src/components/TextArea.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class TextArea extends React.Component {
+  componentDidMount() {
+    const textarea = document.getElementsByClassName('letter-textarea')[0];
+    if (textarea) textarea.style.height = textarea.scrollHeight + 'px';
+  }
+
+  componentDidUpdate() {
+    const textarea = document.getElementsByClassName('letter-textarea')[0];
+    if (textarea) textarea.style.height = textarea.scrollHeight + 'px';
+  }
+
+  render() {
+    const { text, onChange } = this.props;
+
+    return (
+      <textarea
+        className="letter-textarea"
+        value={text}
+        onChange={({ target }) =>
+          onChange({ newContent: target.value })
+        }
+      />
+    );
+  }
+}
+
+TextArea.propTypes = {
+  text: PropTypes.string.isRequired,
+  onChange: PropTypes.func,
+};
+
+export { TextArea };
