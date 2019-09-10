4b43678aff75af26d9a2c7095c8b3778e82ca719
Liv <w.phrush@gmail.com>
2019-09-09 20:04:08 -0500

    update instructions with new variable

--- src/lib/instructions.js
+++ src/lib/instructions.js
@@ -2,7 +2,7 @@ const instructions = `{{salutation}}
 
 Welcome to letter.tools! This site is dedicated to simplifying writing multiple letters with similar, but varying, content.
 
-All you need to do is write up the letter you want, and set anything you want to be injected you should wrap in double curly braces, like this: {{variable}}
+All you need to do is write up the letter you want, and set anything you want to be injected you should wrap in double curly braces, like this: {{example}}
 
 Once you set your variables they should show up in the left side column, and you can use the accompanying inputs to specify the value of each variable.
 
